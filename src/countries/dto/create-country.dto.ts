96cd86e22f69d43ef3cd7217b851c0cb7ad606c5
İlker Özin <ilker.ozin@selebra.net>
2023-09-19 09:43:08 +0300

    added country annotations

--- src/countries/dto/create-country.dto.ts
+++ src/countries/dto/create-country.dto.ts
@@ -1,5 +1,24 @@
+import { IsInt, IsString } from 'class-validator';
+
 export class CreateCountryDto {
+  @IsString()
   readonly name: string;
+
+  @IsInt()
   readonly age: number;
+
+  @IsString()
   readonly breed: string;
+
+  @IsString()
+  readonly alpha2Code: string;
+
+  @IsString()
+  readonly alpha3Code: string;
+
+  @IsString()
+  readonly numericCode: number;
+
+  @IsString()
+  readonly nameTurkish: string;
 }
--- src/countries/schemas/countries.schema.ts
+++ src/countries/schemas/countries.schema.ts
@@ -1,11 +1,16 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import { ApiProperty } from '@nestjs/swagger';
 
 export type CountryDocument = HydratedDocument<Country>;
 
 @Schema()
 export class Country {
   @Prop()
+  @ApiProperty({
+    example: 'Italy',
+    description: 'Name of the country in english.',
+  })
   name: string;
 
   @Prop()
@@ -13,6 +18,34 @@ export class Country {
 
   @Prop()
   breed: string;
+
+  @Prop()
+  @ApiProperty({
+    example: 'IT',
+    description: ' ISO 3166 Alpha2Code of the country.',
+  })
+  alpha2Code: string;
+
+  @Prop()
+  @ApiProperty({
+    example: 'ITA',
+    description: 'ISO 3166 Alpha3Code of the country.',
+  })
+  alpha3Code: string;
+
+  @Prop()
+  @ApiProperty({
+    example: 380,
+    description: 'ISO 3166 NumericCode of the country.',
+  })
+  numericCode: number;
+
+  @Prop()
+  @ApiProperty({
+    example: 'İtalya',
+    description: 'Name of the country in turkish.',
+  })
+  nameTurkish: string;
 }
 
 export const CountrySchema = SchemaFactory.createForClass(Country);
