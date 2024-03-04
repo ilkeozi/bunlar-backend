7e198e8edadd4c401dc723b1d823c64393a8e8fa
İlker Özin <ilker.ozin@selebra.net>
2024-03-04 10:24:51 +0300

    changed swagger documentation to default path

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap(): Promise<Handler> {
     .build();
   const document = SwaggerModule.createDocument(app, config);
 
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('/', app, document);
   /* await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`); */
   await app.init();
